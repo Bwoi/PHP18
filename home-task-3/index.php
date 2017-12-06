a34a957f7a7a64ad200fef80f1f3a2cfdf65987b
Bwoi <bwoi.5000@gmail.com>
2017-12-06 20:37:54 +0300

    правки по ДЗ

--- home-task-3/index.php
+++ home-task-3/index.php
@@ -10,45 +10,82 @@ $continents = [
     "Asia" => ["White Tiger", "Russian Bear", "Lepus europaeus"]
 ];
 
-//Ищем животных из двух слов
-$animalsOfTwoWords = [];
+$newArrContinents = [];
+$secondPart = [];
 foreach ($continents as $continent => $animals) {
     foreach ($animals as $animal) {
-        $slice = explode(' ', $animal);
-        if(count($slice) == 2) {
-            $animalsOfTwoWords[] = $animal;
+        $sliced = explode(' ', $animal);
+        if (count($sliced) == 2) {
+            $newArrContinents[$continent][] = $sliced[0];
+            $secondPart[] = $sliced[1];
         }
     }
 }
+shuffle($secondPart);
 
-//Делим названия на первое и второе слово
-$newAnimals = [
-    'firstPart' => [],
-    'secondPart' => []
-];
-foreach ($animalsOfTwoWords as $animal) {
-    $slice = explode(' ', $animal);
-    $newAnimals['firstPart'][] = $slice[0];
-    $newAnimals['secondPart'][] = $slice[1];
-}
-shuffle($newAnimals['firstPart']);
-shuffle($newAnimals['secondPart']);
-
-//Назначаем рендомно имена
-$randomNames = [];
-foreach ($newAnimals['firstPart'] as $name) {
-    foreach ($continents as $continent => $animals) {
-        foreach ($animals as $animal) {
-            $slice = explode(' ', $animal);
-            if(count($slice) == 2 && $slice[0] == $name) {
-                $randomNames[$continent][] = $name.' '.array_pop($newAnimals['secondPart']);
-            }
-        }
+foreach ($newArrContinents as $continent => $animalsName) {
+    echo "<h2>".$continent."</h2>";
+    $fantasyAnimalNamesOfContinent = [];
+    foreach ($animalsName as $animal) {
+        $fantasyAnimalNamesOfContinent[] = $animal.' '.array_pop($secondPart);
     }
+    echo implode(', ', $fantasyAnimalNamesOfContinent);
 }
 
-//Выводим на экран название животных и место их обитания
-foreach ($randomNames as $country => $animals) {
-    echo "<h2>".$country."</h2>";
-    echo implode(', ', $animals);
-}
+
+
+
+
+
+
+//******************************************************************
+// С Т А Р А Я   Р Е А Л И З А Ц И Я
+//******************************************************************
+
+
+
+
+
+
+////Ищем животных из двух слов
+//$animalsOfTwoWords = [];
+//foreach ($continents as $continent => $animals) {
+//    foreach ($animals as $animal) {
+//        $slice = explode(' ', $animal);
+//        if(count($slice) == 2) {
+//            $animalsOfTwoWords[] = $animal;
+//        }
+//    }
+//}
+//
+////Делим названия на первое и второе слово
+//$newAnimals = [
+//    'firstPart' => [],
+//    'secondPart' => []
+//];
+//foreach ($animalsOfTwoWords as $animal) {
+//    $slice = explode(' ', $animal);
+//    $newAnimals['firstPart'][] = $slice[0];
+//    $newAnimals['secondPart'][] = $slice[1];
+//}
+//shuffle($newAnimals['firstPart']);
+//shuffle($newAnimals['secondPart']);
+//
+////Назначаем рендомно имена
+//$randomNames = [];
+//foreach ($newAnimals['firstPart'] as $name) {
+//    foreach ($continents as $continent => $animals) {
+//        foreach ($animals as $animal) {
+//            $slice = explode(' ', $animal);
+//            if(count($slice) == 2 && $slice[0] == $name) {
+//                $randomNames[$continent][] = $name.' '.array_pop($newAnimals['secondPart']);
+//            }
+//        }
+//    }
+//}
+//
+////Выводим на экран название животных и место их обитания
+//foreach ($randomNames as $country => $animals) {
+//    echo "<h2>".$country."</h2>";
+//    echo implode(', ', $animals);
+//}
